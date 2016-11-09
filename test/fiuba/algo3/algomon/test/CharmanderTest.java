ed0194982f44201fe2a0456012f07c06da73dea0
kopoz <lucas.sirio.bonastre@gmail.com>
2016-11-09 04:18:52 +0200

    Add files via upload
    Cambios en CharmanderTest.java
    -Se agrega un Test de ataque Brasas a un pokemon de tipo normal
    -Se corrige la indentacion

--- test/fiuba/algo3/algomon/test/CharmanderTest.java
+++ test/fiuba/algo3/algomon/test/CharmanderTest.java
@@ -6,33 +6,47 @@ import static org.junit.Assert.*;
 
 public class CharmanderTest {
 
-	    @Test
-	    public void brasasQuita32PuntosDeVidaABulbasaur()
-	    {
-	        AlgoMon charmander = Especie.CHARMANDER.nuevo();
-	        AlgoMon bulbasaur = Especie.BULBASAUR.nuevo();
+	@Test
+	public void brasasQuita32PuntosDeVidaABulbasaur()
+	{
+		AlgoMon charmander = Especie.CHARMANDER.nuevo();
+		AlgoMon bulbasaur = Especie.BULBASAUR.nuevo();
 
-	        int vidaInicial = bulbasaur.vida();
+		int vidaInicial = bulbasaur.vida();
 
-	        charmander.atacar(bulbasaur, Movimiento.BRASAS);
+		charmander.atacar(bulbasaur, Movimiento.BRASAS);
 
-	        assertEquals(vidaInicial - 32, bulbasaur.vida());
-	    }
-
-	    @Test
-	    public void brasasQuita8PuntosDeVidaASquirtle()
-	    {
-	        AlgoMon charmander = Especie.CHARMANDER.nuevo();
-	        AlgoMon squirtle = Especie.SQUIRTLE.nuevo();
-
-	        int vidaInicial = squirtle.vida();
-
-	        charmander.atacar(squirtle, Movimiento.BRASAS);
-
-	        assertEquals(vidaInicial - 8, squirtle.vida());
-	    }
-
-
+		assertEquals(vidaInicial - 32, bulbasaur.vida());
 	}
+
+	@Test
+	public void brasasQuita8PuntosDeVidaASquirtle()
+	{
+		AlgoMon charmander = Especie.CHARMANDER.nuevo();
+		AlgoMon squirtle = Especie.SQUIRTLE.nuevo();
+
+		int vidaInicial = squirtle.vida();
+
+		charmander.atacar(squirtle, Movimiento.BRASAS);
+
+		assertEquals(vidaInicial - 8, squirtle.vida());
+	}
+
+	@Test
+	public void brasasQuita16PuntosDeVidaARattata()
+	{
+		AlgoMon charmander = Especie.CHARMANDER.nuevo();
+		AlgoMon rattata = Especie.RATTATA.nuevo();
+
+		int vidaInicial = rattata.vida();
+
+		charmander.atacar(rattata, Movimiento.BRASAS);
+
+		assertEquals(vidaInicial - 16, rattata.vida());
+	}
+
+
+
+}
 
 
