8430b9f88682190da2817ceeae619c37948e62a9
pabloernesto <caussabon@gmail.com>
2016-11-09 20:33:48 -0300

    cambiada la firma del constructor de algomon

--- src/fiuba/algo3/algomon/AlgoMon.java
+++ src/fiuba/algo3/algomon/AlgoMon.java
@@ -8,16 +8,18 @@ import java.util.EnumMap;
 
 public class AlgoMon
 {
-    int vida = 100;
+    int vida;
     Tipo tipo;
     Map<Movimiento, Ataque> ataques = new EnumMap(Movimiento.class);
 
-    public AlgoMon(Tipo t, Movimiento [] movimientos)
+    public AlgoMon(Tipo t, Movimiento [] movimientos, int vida)
     {
         tipo = t;
 
         for (Movimiento m : movimientos)
             ataques.put(m, new Ataque(m));
+
+        this.vida = vida;
     }
 
     public AlgoMon atacar(AlgoMon enemigo, Movimiento movimiento)
