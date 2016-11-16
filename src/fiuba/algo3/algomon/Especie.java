337c92a394bf45d12cdf06e03d1030801ebf563d
Jumelen <jaromero.2407@gmail.com>
2016-11-16 18:32:00 -0300

    Pequeño refactor para hacer más legible la clase.

--- src/fiuba/algo3/algomon/Especie.java
+++ src/fiuba/algo3/algomon/Especie.java
@@ -2,69 +2,21 @@ package fiuba.algo3.algomon;
 
 public enum Especie
 {
-    SQUIRTLE(
-        Tipo.AGUA,
-        new Movimiento[]
-        {
-            Movimiento.BURBUJA,
-            Movimiento.CANION_DE_AGUA,
-            Movimiento.ATAQUE_RAPIDO
-        },
-        150),
-    CHARMANDER(
-        Tipo.FUEGO,
-        new Movimiento[]
-        {
-            Movimiento.BRASAS,
-            Movimiento.FOGONAZO,
-            Movimiento.ATAQUE_RAPIDO
-        },
-        170),
-    BULBASAUR(
-        Tipo.PLANTA,
-        new Movimiento[]
-        {
-            Movimiento.CHUPAVIDAS,
-            Movimiento.LATIGO_CEPA,
-            Movimiento.ATAQUE_RAPIDO
-        },
-        140),
-    RATTATA(
-        Tipo.NORMAL,
-        new Movimiento[]
-        {
-            Movimiento.FOGONAZO,
-            Movimiento.BURBUJA,
-            Movimiento.ATAQUE_RAPIDO
-        },
-        170),
-    CHANSEY(
-        Tipo.NORMAL,
-        new Movimiento[]
-        {
-            Movimiento.CANTO,
-            Movimiento.LATIGO_CEPA,
-            Movimiento.ATAQUE_RAPIDO
-        },
-        130),
-    JIGGLYPUFF(
-        Tipo.NORMAL,
-        new Movimiento[]
-        {
-            Movimiento.CANTO,
-            Movimiento.BURBUJA,
-            Movimiento.ATAQUE_RAPIDO
-        },
-        130);
+    SQUIRTLE	(Tipo.AGUA,   new Movimiento[] { Movimiento.BURBUJA, Movimiento.CANION_DE_AGUA, Movimiento.ATAQUE_RAPIDO }, 150),
+    CHARMANDER	(Tipo.FUEGO,  new Movimiento[] { Movimiento.BRASAS, Movimiento.FOGONAZO, Movimiento.ATAQUE_RAPIDO }, 170),
+    BULBASAUR	(Tipo.PLANTA, new Movimiento[] { Movimiento.CHUPAVIDAS, Movimiento.LATIGO_CEPA, Movimiento.ATAQUE_RAPIDO }, 140),
+    RATTATA		(Tipo.NORMAL, new Movimiento[] { Movimiento.FOGONAZO, Movimiento.BURBUJA, Movimiento.ATAQUE_RAPIDO }, 170),
+    CHANSEY		(Tipo.NORMAL, new Movimiento[] { Movimiento.CANTO, Movimiento.LATIGO_CEPA, Movimiento.ATAQUE_RAPIDO }, 130),
+    JIGGLYPUFF	(Tipo.NORMAL, new Movimiento[] { Movimiento.CANTO, Movimiento.BURBUJA, Movimiento.ATAQUE_RAPIDO }, 130);
 
     Movimiento[] movimientos;
     Tipo tipo;
     int vida;
 
-    Especie(Tipo t, Movimiento[] m, int vida) {
+    Especie(Tipo t, Movimiento[] m, int v) {
         this.tipo = t;
         this.movimientos = m;
-        this.vida = vida;
+        this.vida = v;
     }
 
     public AlgoMon nuevo() {
