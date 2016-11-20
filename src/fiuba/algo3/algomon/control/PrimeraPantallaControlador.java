e4f99771842cffd7c353eb786a5a030a870ce4f2
Jumelen <jaromero.2407@gmail.com>
2016-11-19 21:19:13 -0300

    Pantalla principal creada!

---
+++ src/fiuba/algo3/algomon/control/PrimeraPantallaControlador.java
@@ -0,0 +1,29 @@
+package fiuba.algo3.algomon.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import fiuba.algo3.algomon.vista.Ejecutar;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class PrimeraPantallaControlador implements Initializable{
+	
+	private Ejecutar programaPrincipal;
+	
+	@FXML
+    private void nuevaVentana(ActionEvent evento) {
+        //llamado desde el botón que existe en la vista.
+    }
+	
+	public void setProgramaPrincipal(Ejecutar programaPrincipal) {
+		this.programaPrincipal = programaPrincipal;		
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO
+	}
+
+}
---
+++ src/fiuba/algo3/algomon/vista/Ejecutar.java
@@ -0,0 +1,46 @@
+package fiuba.algo3.algomon.vista;
+
+import java.io.IOException;
+
+import fiuba.algo3.algomon.control.PrimeraPantallaControlador;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Ejecutar extends Application {
+	 
+    private Stage stagePrincipal;
+    private BorderPane rootPane;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stagePrincipal) throws Exception {
+        this.stagePrincipal = stagePrincipal;
+        this.mostrarVentanaPrincipal();
+    }
+ 
+    public void mostrarVentanaPrincipal() throws IOException {
+
+    	FXMLLoader loader = new FXMLLoader(Ejecutar.class.getResource("PrimeraPantalla.fxml"));
+    	loader.setController(new PrimeraPantallaControlador());
+    	this.rootPane = (BorderPane) loader.load();
+    	
+		Scene escena = new Scene(rootPane,1400,900,Color.BLUE);
+		stagePrincipal.setTitle("Algomong 1.0 BETA Version");
+		stagePrincipal.setScene(escena);
+    	
+		PrimeraPantallaControlador controlador = loader.getController();
+		controlador.setProgramaPrincipal(this);
+			
+		stagePrincipal.show();
+		
+    }
+    
+    
+}
