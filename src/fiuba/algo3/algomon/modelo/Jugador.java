6331df6086dbc0b1eab585aea48e3c8c0369790e
pabloernesto <caussabon@gmail.com>
2016-11-20 19:34:58 -0300

    cambio de indentacion

--- src/fiuba/algo3/algomon/modelo/Jugador.java
+++ src/fiuba/algo3/algomon/modelo/Jugador.java
@@ -6,45 +6,45 @@ import fiuba.algo3.algomon.excepciones.ImposibleElegirMasDeTresAlgomonesExceptio
 
 public class Jugador {
 
-	private static final int MAX_CANT_ALGOMONES_POR_JUGADOR = 3;
-	
-	private String nombre;
-	private ArrayList<Algomon> algomones;
+    private static final int MAX_CANT_ALGOMONES_POR_JUGADOR = 3;
 
-	public Jugador(String nombreJugador) {
-		this.nombre = nombreJugador;
-		this.algomones = new ArrayList<Algomon>();
-	}
+    private String nombre;
+    private ArrayList<Algomon> algomones;
 
-	public void elegirAlgomon(Algomon algomon) {
-		int i = 0;
-		
-		while (i < MAX_CANT_ALGOMONES_POR_JUGADOR){
-			if (this.algomones.size() == i){
-				this.algomones.add(i, algomon);
-				break;
-			}
-			i++;
-		}
-		
-		if (i == MAX_CANT_ALGOMONES_POR_JUGADOR)
-			throw new ImposibleElegirMasDeTresAlgomonesException();
-		
-	}
+    public Jugador(String nombreJugador) {
+        this.nombre = nombreJugador;
+        this.algomones = new ArrayList<Algomon>();
+    }
 
-	public Algomon algomonNro(int nroDeAlgomon) {
-		return this.algomones.get(nroDeAlgomon-1);
-	}
+    public void elegirAlgomon(Algomon algomon) {
+        int i = 0;
 
-	public String nombre() {
-		return this.nombre;
-	}
+        while (i < MAX_CANT_ALGOMONES_POR_JUGADOR){
+            if (this.algomones.size() == i){
+                this.algomones.add(i, algomon);
+                break;
+            }
+            i++;
+        }
 
-	public void actualizarEstadoAlgomones() {
-		for (int i = 0; i < MAX_CANT_ALGOMONES_POR_JUGADOR; i++){
-			this.algomones.get(i).turnoTerminado();
-		}
-	}
+        if (i == MAX_CANT_ALGOMONES_POR_JUGADOR)
+            throw new ImposibleElegirMasDeTresAlgomonesException();
+
+    }
+
+    public Algomon algomonNro(int nroDeAlgomon) {
+        return this.algomones.get(nroDeAlgomon-1);
+    }
+
+    public String nombre() {
+        return this.nombre;
+    }
+
+    public void actualizarEstadoAlgomones() {
+        for (int i = 0; i < MAX_CANT_ALGOMONES_POR_JUGADOR; i++){
+            this.algomones.get(i).turnoTerminado();
+        }
+    }
 
 
 }
