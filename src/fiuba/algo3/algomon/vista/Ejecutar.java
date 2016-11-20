6d260c6bdbc06dc6bfb4460b6bc622e0d9eb33ca
pabloernesto <caussabon@gmail.com>
2016-11-20 17:11:52 -0300

    arreglado ant ejecutar.aplicacion

--- src/fiuba/algo3/algomon/vista/Ejecutar.java
+++ src/fiuba/algo3/algomon/vista/Ejecutar.java
@@ -9,38 +9,46 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import java.net.URL;
 
 public class Ejecutar extends Application {
-	 
+
     private Stage stagePrincipal;
     private BorderPane rootPane;
 
     public static void main(String[] args) {
         launch(args);
     }
-    
+
     @Override
     public void start(Stage stagePrincipal) throws Exception {
         this.stagePrincipal = stagePrincipal;
         this.mostrarVentanaPrincipal();
     }
- 
+
     public void mostrarVentanaPrincipal() throws IOException {
 
-    	FXMLLoader loader = new FXMLLoader(Ejecutar.class.getResource("PrimeraPantalla.fxml"));
-    	loader.setController(new PrimeraPantallaControlador());
-    	this.rootPane = (BorderPane) loader.load();
-    	
-		Scene escena = new Scene(rootPane,1400,900,Color.BLUE);
-		stagePrincipal.setTitle("Algomong 1.0 BETA Version");
-		stagePrincipal.setScene(escena);
-    	
-		PrimeraPantallaControlador controlador = loader.getController();
-		controlador.setProgramaPrincipal(this);
-			
-		stagePrincipal.show();
-		
+        FXMLLoader loader;
+
+        {
+            URL pantallaURL =
+                Ejecutar.class.getResource("xml/PrimeraPantalla.fxml");
+            loader = new FXMLLoader(pantallaURL);
+        }
+
+        loader.setController(new PrimeraPantallaControlador());
+        this.rootPane = (BorderPane) loader.load();
+
+        Scene escena = new Scene(rootPane,1400,900,Color.BLUE);
+        stagePrincipal.setTitle("Algomong 1.0 BETA Version");
+        stagePrincipal.setScene(escena);
+
+        PrimeraPantallaControlador controlador = loader.getController();
+        controlador.setProgramaPrincipal(this);
+
+        stagePrincipal.show();
+
     }
-    
-    
+
+
 }
