f8fcfd1bae893f381d2229b828cc716b5eda3be0
pabloernesto <caussabon@gmail.com>
2016-11-20 18:44:11 -0300

    eliminada dependencia de fxml

--- src/fiuba/algo3/algomon/vista/Ejecutar.java
+++ src/fiuba/algo3/algomon/vista/Ejecutar.java
@@ -2,14 +2,11 @@ package fiuba.algo3.algomon.vista;
 
 import java.io.IOException;
 
-import fiuba.algo3.algomon.control.PrimeraPantallaControlador;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import java.net.URL;
 
 public class Ejecutar extends Application {
 
@@ -28,23 +25,11 @@ public class Ejecutar extends Application {
 
     public void mostrarVentanaPrincipal() throws IOException {
 
-        FXMLLoader loader;
-
-        {
-            URL pantallaURL =
-                Ejecutar.class.getResource("xml/PrimeraPantalla.fxml");
-            loader = new FXMLLoader(pantallaURL);
-        }
-
-        loader.setController(new PrimeraPantallaControlador());
-        this.rootPane = (BorderPane) loader.load();
+        rootPane = new BorderPane();
 
         Scene escena = new Scene(rootPane,300,275,Color.BLUE);
         stage.setTitle("Algomong 1.0 BETA Version");
         stage.setScene(escena);
-
-        PrimeraPantallaControlador controlador = loader.getController();
-        controlador.setProgramaPrincipal(this);
 
         stage.show();
     }
