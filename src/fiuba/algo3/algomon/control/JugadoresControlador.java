8a28ff60fe196b54586957cf16665aa953a7dc22
pabloernesto <caussabon@gmail.com>
2016-11-29 18:59:59 -0300

    eliminados efectos secundarios de un constructor

--- src/fiuba/algo3/algomon/control/JugadoresControlador.java
+++ src/fiuba/algo3/algomon/control/JugadoresControlador.java
@@ -1,14 +1,27 @@
 package fiuba.algo3.algomon.control;
 
+import fiuba.algo3.algomon.modelo.Juego;
 import fiuba.algo3.algomon.modelo.Jugador;
 import fiuba.algo3.algomon.vista.PantallaJugador;
 
 public class JugadoresControlador {
 
-	public JugadoresControlador(PantallaJugador pantallaJugador, Ejecutar aplicacion){
-		
-		aplicacion.getModelo().agregarJugador(new Jugador(pantallaJugador.txtNombreJugador1.getText()));
-		aplicacion.getModelo().agregarJugador(new Jugador(pantallaJugador.txtNombreJugador2.getText()));
-		
-	}
+    Juego j;
+    PantallaJugador p;
+
+    public JugadoresControlador(PantallaJugador pantallaJugador,
+        Ejecutar aplicacion){
+
+        j = aplicacion.getModelo();
+        p = pantallaJugador;
+
+    }
+
+    public void agregarJugadores() {
+        String n1 = p.txtNombreJugador1.getText();
+        String n2 = p.txtNombreJugador2.getText();
+
+        j.agregarJugador(new Jugador(n1));
+        j.agregarJugador(new Jugador(n2));
+    }
 }
--- src/fiuba/algo3/algomon/vista/PantallaJugador.java
+++ src/fiuba/algo3/algomon/vista/PantallaJugador.java
@@ -12,37 +12,38 @@ import javafx.stage.Stage;
 
 public class PantallaJugador extends Scene {
 
-	private Ejecutar aplicacion;
-	static FlowPane rootPane = new FlowPane();
-	public TextField txtNombreJugador1,txtNombreJugador2;
-	
-	public PantallaJugador(Stage ventana, Ejecutar aplicacion) {
+    private Ejecutar aplicacion;
+    static FlowPane rootPane = new FlowPane();
+    public TextField txtNombreJugador1,txtNombreJugador2;
+
+    public PantallaJugador(Stage ventana, Ejecutar aplicacion) {
         super(rootPane,480,480);
         this.aplicacion = aplicacion;
 
         mostrarPantallaJugadores();
     }
 
-	private void mostrarPantallaJugadores() {
-		
-		HBox hboxJugador1 = new HBox();
-			Label lblNombreJugador1 = new Label("Nombre de entrenador/a Algomon N1: ");
-	        txtNombreJugador1 = new TextField();
+    private void mostrarPantallaJugadores() {
+
+        HBox hboxJugador1 = new HBox();
+            Label lblNombreJugador1 = new Label("Nombre de entrenador/a Algomon N1: ");
+            txtNombreJugador1 = new TextField();
         hboxJugador1.getChildren().addAll(lblNombreJugador1, txtNombreJugador1);
-        
-		HBox hboxJugador2 = new HBox();
-			Label lblNombreJugador2 = new Label("Nombre de entrenador/a Algomon N2: ");
-	        txtNombreJugador2 = new TextField();
+
+        HBox hboxJugador2 = new HBox();
+            Label lblNombreJugador2 = new Label("Nombre de entrenador/a Algomon N2: ");
+            txtNombreJugador2 = new TextField();
         hboxJugador2.getChildren().addAll(lblNombreJugador2, txtNombreJugador2);
 
         Button btnContinuar = new Button("Continuar");
         btnContinuar.setOnAction(e -> {
-        	new JugadoresControlador(this, aplicacion);
-        	aplicacion.crearPantallaEleccionAlgomones();
+            JugadoresControlador c = new JugadoresControlador(this, aplicacion);
+            c.agregarJugadores();
+            aplicacion.crearPantallaEleccionAlgomones();
         });
-        
+
         rootPane.getChildren().addAll(hboxJugador1,hboxJugador2,btnContinuar);
         rootPane.setStyle("-fx-background-color: #336699;");
-	}
+    }
 
 }
