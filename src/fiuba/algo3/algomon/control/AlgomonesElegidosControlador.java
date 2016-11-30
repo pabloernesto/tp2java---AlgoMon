d32555cb4e11bd9cb38f717a1a15a96bf6036ecc
Jumelen <jaromero.2407@gmail.com>
2016-11-30 12:57:03 -0300

    Merge

--- src/fiuba/algo3/algomon/control/AlgomonesElegidosControlador.java
+++ src/fiuba/algo3/algomon/control/AlgomonesElegidosControlador.java
@@ -2,13 +2,32 @@ package fiuba.algo3.algomon.control;
 
 import java.util.ArrayList;
 
+import fiuba.algo3.algomon.excepciones.SeDebenElegirTresAlgomonesPorJugadorException;
 import fiuba.algo3.algomon.vista.PantallaEleccionAlgomones;
 
 public class AlgomonesElegidosControlador {
 
+	private Ejecutar aplicacion;
+	private ArrayList<String> algomonesElegidos;
+
 	public AlgomonesElegidosControlador(PantallaEleccionAlgomones pantallaEleccionAlgomones, Ejecutar aplicacion,
-			ArrayList<String> algomones, int nroDeJugador) {
-		// TODO Auto-generated constructor stub
+			ArrayList<String> algomones, int nroDeJugador) throws SeDebenElegirTresAlgomonesPorJugadorException {
+		this.aplicacion = aplicacion;
+		this.algomonesElegidos = algomones;
+		
+		if (algomones.size() != 3){
+			throw new SeDebenElegirTresAlgomonesPorJugadorException();
+		}else{
+			agregarAlgomonesA(nroDeJugador);
+		}
+	}
+
+	private void agregarAlgomonesA(int nroDeJugador) {
+		for(int i = 0; i <= 2; i++){
+			switch (algomonesElegidos.get(i)) {
+			case "Squirtle": aplicacion.getModelo();
+			}
+		}
 	}
 
 }
---
+++ src/fiuba/algo3/algomon/excepciones/SeDebenElegirTresAlgomonesPorJugadorException.java
@@ -0,0 +1,10 @@
+package fiuba.algo3.algomon.excepciones;
+
+public class SeDebenElegirTresAlgomonesPorJugadorException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
