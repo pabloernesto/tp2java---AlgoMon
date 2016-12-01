654708de6ccdbd955057bdaff5c6bcdff1aab6ae
Kopoz <lucas.sirio.bonastre@gmail.com>
2016-12-01 16:57:03 -0300

    se agrega el constructor implicito para mantener compatibilidad

--- src/fiuba/algo3/algomon/modelo/elementos/Item.java
+++ src/fiuba/algo3/algomon/modelo/elementos/Item.java
@@ -5,6 +5,7 @@ import fiuba.algo3.algomon.modelo.Algomon;
 public abstract class Item {
     int cantidadDeUsosRestantes;
     private Elemento elemento;
+    public Item(){};
 
     public Item(Elemento unElemento) {
         this.cantidadDeUsosRestantes = unElemento.cantidadInicialDeElemento;
