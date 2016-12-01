e3e5aa8d4b09b4fba12ee29ec2a13d4e9cb83ed4
pabloernesto <caussabon@gmail.com>
2016-12-01 18:23:15 -0300

    desactivado el sonido

--- src/fiuba/algo3/algomon/control/Sonido.java
+++ src/fiuba/algo3/algomon/control/Sonido.java
@@ -6,11 +6,11 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class Sonido {
-	
+
     MediaPlayer player;
-    
-    	public Sonido(){
-    		String uriString = new File("sounds/Pokemon_theme_song.mp3").toURI().toString();
-    		player = new MediaPlayer( new Media(uriString));
-    	}
+
+        public Sonido(){
+            //~ String uriString = new File("sounds/Pokemon_theme_song.mp3").toURI().toString();
+            //~ player = new MediaPlayer( new Media(uriString));
+        }
     }
