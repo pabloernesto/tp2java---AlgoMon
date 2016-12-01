5bb822912acfed6514890d961695e7383eba1dac
Kopoz <lucas.sirio.bonastre@gmail.com>
2016-12-01 16:55:19 -0300

    Refactor de item se delega responsabilidad comun a los Item

--- src/fiuba/algo3/algomon/modelo/elementos/Item.java
+++ src/fiuba/algo3/algomon/modelo/elementos/Item.java
@@ -4,6 +4,12 @@ import fiuba.algo3.algomon.modelo.Algomon;
 
 public abstract class Item {
     int cantidadDeUsosRestantes;
+    private Elemento elemento;
+
+    public Item(Elemento unElemento) {
+        this.cantidadDeUsosRestantes = unElemento.cantidadInicialDeElemento;
+        this.elemento = unElemento;
+    }
 
     public abstract void aplicar(Algomon algomonActivo);
 
