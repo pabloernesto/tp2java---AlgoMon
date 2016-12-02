d988bfe72b5e5df66d583414a4144c0e38b355e3
pabloernesto <caussabon@gmail.com>
2016-12-02 18:44:16 -0300

    atrapada la excepcion AlgomonEstaInhabilitadoParaAtacar

--- src/fiuba/algo3/algomon/control/AtaqueControlador.java
+++ src/fiuba/algo3/algomon/control/AtaqueControlador.java
@@ -3,8 +3,12 @@ package fiuba.algo3.algomon.control;
 import fiuba.algo3.algomon.modelo.Algomon;
 import fiuba.algo3.algomon.modelo.Juego;
 import fiuba.algo3.algomon.modelo.ataque.Ataque;
+
 import fiuba.algo3.algomon.vista.AtaqueVistaBoton;
 import fiuba.algo3.algomon.vista.OpcionesJugador;
+
+import fiuba.algo3.algomon.excepciones.*;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
@@ -22,12 +26,18 @@ public class AtaqueControlador implements EventHandler<ActionEvent> {
 
     @Override
     public void handle(ActionEvent event) {
-            Juego j = Juego.instancia();
-            Algomon atacante = j.obtenerJugadorActivo().getAlgomonActivo();
-            Algomon enemigo = j.jugadorNoActivo().getAlgomonActivo();
+        Juego j = Juego.instancia();
+        Algomon atacante = j.obtenerJugadorActivo().getAlgomonActivo();
+        Algomon enemigo = j.jugadorNoActivo().getAlgomonActivo();
+
+        try {
             atacante.atacar(enemigo, ataque.movimiento());
-            j.pasarTurno();
-            panel.getChildren().clear();
-            panel.getChildren().add(new OpcionesJugador(panel));
+        }
+        catch (AlgomonEstaInhabilitadoParaAtacarException e) {
+            //~ ???
+        }
+        j.pasarTurno();
+        panel.getChildren().clear();
+        panel.getChildren().add(new OpcionesJugador(panel));
     }
 }
