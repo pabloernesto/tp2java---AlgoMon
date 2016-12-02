980bfb2cb1009b87680506db08515fa918cfd424
Kopoz <lucas.sirio.bonastre@gmail.com>
2016-12-01 23:32:43 -0300

    se agrega la logia al AlgomonoCambioControlador

--- src/fiuba/algo3/algomon/control/AlgomonCambioControlador.java
+++ src/fiuba/algo3/algomon/control/AlgomonCambioControlador.java
@@ -2,6 +2,7 @@ package fiuba.algo3.algomon.control;
 
 import fiuba.algo3.algomon.modelo.Algomon;
 import fiuba.algo3.algomon.modelo.Juego;
+import fiuba.algo3.algomon.modelo.Jugador;
 import fiuba.algo3.algomon.vista.AlgomonVistaBoton;
 import fiuba.algo3.algomon.vista.OpcionesJugador;
 import javafx.event.ActionEvent;
@@ -24,7 +25,8 @@ public class AlgomonCambioControlador implements EventHandler<ActionEvent> {
     @Override
     // todavia no esta la logica del cambio de algomon;
     public void handle(ActionEvent arg0) {
-        Juego.instancia().pasarTurno();
+        Jugador jugador =Juego.instancia().obtenerJugadorActivo();
+        jugador.cambiarAlgomonActivo(this.algomon);
         panel.getChildren().clear();
         panel.getChildren().add(new OpcionesJugador(panel));
     }
